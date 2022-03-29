51db52c80b0b82c0a5018221f5ede03b74c3c11f
Samikshagurav <guravsamiksha17@gmail.com>
2022-03-29 14:04:51 +0530

    first commit

---
+++ Practice/Map.js
@@ -0,0 +1,15 @@
+const myAwesomeArray = [3,5,4,2]
+
+// myAwesomeArray.map((element,index) =>
+//      element
+// ).filter(ele=>ele<4)
+// console.log(data);
+let data= myAwesomeArray.map(ele=>
+     ele*ele
+     ).filter(ele=>ele<10)
+     console.log(data);
+
+//  data.push(myAwesomeArray)
+//  data =data.concat(myAwesomeArray)
+//  console.log(data[0])
+//  console.log(data[0][1]);
